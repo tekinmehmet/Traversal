d8e71db0ebbd4379cf885c1a5d0d54855694a63e
tekimmehmet <tekimmehmet@hotmail.com>
2024-01-19 15:22:56 +0300

    Tur rota blog başlangıç

---
+++ Traversal/Controllers/DestinationController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traversal.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EFDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetail(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetail(Destination p)
+        {
+            return View();
+        }
+    }
+}
